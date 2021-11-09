40825153a4fe6a5bad05703276c47393111dea0c
LiJianying <lijy91@foxmail.com>
2021-11-09 23:18:09 +0800

    Published anyinspect_plugin_shared_preferences 0.0.2

--- packages/anyinspect_plugin_shared_preferences/lib/src/anyinspect_plugin_shared_preferences.dart
+++ packages/anyinspect_plugin_shared_preferences/lib/src/anyinspect_plugin_shared_preferences.dart
@@ -1,26 +1,32 @@
 import 'package:anyinspect_client/anyinspect_client.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
-class AnyInspectPluginSharedPreferences extends AnyInspectPlugin {
+class AnyInspectPluginSharedPreferences extends AnyInspectPlugin
+    with AnyInspectPluginMethodHandler {
   @override
   String get id => 'shared_preferences';
 
   AnyInspectPluginSharedPreferences() {
-    receive('getAll', (data) => getAll());
+    addMethodHandler(this);
   }
 
-  void getAll() async {
+  @override
+  Future<void> handleMethod(AnyInspectPluginMethod method) async {
+    switch (method.name) {
+      case 'getAll':
+        await getAll();
+        return;
+      default:
+    }
+  }
+
+  Future<void> getAll() async {
     SharedPreferences _prefs = await SharedPreferences.getInstance();
-
     List<Map<String, dynamic>> list = [];
-
     Set<String> keys = _prefs.getKeys();
     for (var key in keys) {
-      list.add({
-        'key': key,
-        'value': _prefs.get(key),
-      });
+      list.add({'key': key, 'value': _prefs.get(key)});
     }
-    send('getAllResult', {'list': list});
+    emitEvent('getAllSuccess', {'list': list});
   }
 }
